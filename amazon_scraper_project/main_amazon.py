f821723d02e73840f4eb76609865c380f2a499dd
Prathik Kulkarni <kulkarniprathik04@gmail.com>
2025-08-29 16:37:39 +0530

    Removed flipkart_scraper_project folder from repo

--- amazon_scraper_project/main_amazon.py
+++
@@ -1,9 +0,0 @@
-from data_extractor import run_extraction
-
-# --- Config ---
-SEARCH_QUERY = "washing machine"
-OUTPUT_FILE = r"D:\Day_Task\amazon_scraper_project\products.csv"
-TOTAL_PAGES = 16  # You can adjust this as needed
-
-if __name__ == "__main__":
-    run_extraction(SEARCH_QUERY, OUTPUT_FILE, total_pages=TOTAL_PAGES)
--- flipkart_scraper_project/main.py
+++
@@ -1,8 +0,0 @@
-from data_extractor import run_extraction
-
-if __name__ == "__main__":
-    SEARCH_QUERY = "washing machine"
-    OUTPUT_FILE = r"D:\Day_Task\flipkart_scraper_project\products.csv"
-
-    run_extraction(SEARCH_QUERY, OUTPUT_FILE)
-    
